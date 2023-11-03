bb62940e724a1a453cffafdc8a2ff57af51d4399
Pooja-Gupta07 <poojagupta7775@gmail.com>
2023-11-03 11:34:26 +0530

    commit

---
+++ app/src/main/java/com/example/pqry/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.pqry;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.pqry.MainActivity2;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void NormalExercise(View view) {
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
+    }
+
+
+    public void PregnancyExercise(View view) {
+        Intent intentt = new Intent(this, MainActivity3.class);
+        startActivity(intentt);
+    }
+}
